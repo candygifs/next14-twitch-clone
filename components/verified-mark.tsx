8175f0b72a9231a235817a9079e92e776fe2f9b8
candygifs <150178731+candygifs@users.noreply.github.com>
2025-01-08 01:25:34 +0300

    asdasdas

--- components/verified-mark.tsx
+++ components/verified-mark.tsx
@@ -1,9 +1,18 @@
 import { Check } from "lucide-react";
 
-export const VerifiedMark = () => {
+const VERIFIED_USERS = [
+  'hatikoinside',
+  'user_2rGryq0M83Xv24oO53FJ4X4ozCa'
+];
+
+export const VerifiedMark = ({ userId }: { userId: string }) => {
+  if (!VERIFIED_USERS.includes(userId)) {
+    return null;
+  }
+
   return (
     <div className="p-0.5 flex items-center justify-center h-4 w-4 rounded-full bg-blue-600">
       <Check className="h-[10px] w-[10px] text-primary stroke-[4px]" />
     </div>
-  )
-}
+  );
+};
